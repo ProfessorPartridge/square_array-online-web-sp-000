36bfa16df63e6d80d4fb8f86a4ba7331c459deaa
Josiah  Partridge <jpartridj94@gmail.com>
2020-03-03 03:33:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  array.each do |index|
+    squared_valued = index ** 2
+    new_array = []
+    new_array.last(squared_valued)
+  end
 end
+
+example_array = [1, 2, 3]
+square_array(example_array)
+
+  array = [1, 2, 3]
+  array.each do |index|
+    return squared_valued = index ** 2
+  end
