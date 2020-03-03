4a264fbbc5109994c5ceb06ff6cb28dbddcc5f5e
Josiah  Partridge <jpartridj94@gmail.com>
2020-03-03 03:37:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
+      new_array = []
   array.each do |index|
     squared_valued = index ** 2
-    new_array = []
     new_array.last(squared_valued)
     puts new_array
   end
