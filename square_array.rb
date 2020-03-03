c725d72cf0d2852265ce395a8dff49dc77fe9035
Josiah  Partridge <jpartridj94@gmail.com>
2020-03-03 03:40:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   array.each do |index|
     squared_valued = index ** 2
-    new_array.unshift(squared_valued)
+    new_array.unshift(squared_valued).reverse
   end
   new_array
 end
